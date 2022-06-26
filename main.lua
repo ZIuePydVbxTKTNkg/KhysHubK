2eb95ed17cbcb07b1e7b7cb96b5871b331a8e9b2
ZIuePydVbxTKTNkg <107962780+ZIuePydVbxTKTNkg@users.noreply.github.com>
2022-06-26 09:23:13 -1000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+getgenv().Key = "adad";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZIuePydVbxTKTNkg/KhysHubK/main/Bypasser.lua"))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/CoderMan123/BossMan/main/NewHost"),true))()
+
+-- ez crack
